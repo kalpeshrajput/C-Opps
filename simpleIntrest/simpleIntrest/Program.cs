f34cd02bc244fdf75e60f58435ed25cf07a7a384
Kalpesh rajput <34918443+kalpeshrajput@users.noreply.github.com>
2021-01-31 16:16:19 +0530

    Add files via upload

---
+++ simpleIntrest/simpleIntrest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace simpleIntrest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         getValue();
+        }
+        static void getValue()
+        {
+            simpleInterest objInterest = new simpleInterest();
+            Console.WriteLine("Enter the Loan Ammount");
+            objInterest.principleAmt = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Year");
+            objInterest.year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the rate of Interest");
+            objInterest.rate = Convert.ToDouble(Console.ReadLine());
+            printValues(objInterest);
+        }
+        static void printValues(simpleInterest objInterest) {
+            objInterest.interest = objInterest.principleAmt * objInterest.year * objInterest.rate / 100;
+            objInterest.total_Amt = objInterest.principleAmt + objInterest.interest;
+           
+            Console.WriteLine("Total Ammount = " + objInterest.interest);
+            Console.WriteLine("Total Ammount = " + objInterest.total_Amt);
+        }
+    }
+    public class simpleInterest
+    {
+        internal int year { get; set; }
+        internal double principleAmt { get; set; }
+        internal double interest { get; set; }
+        internal double rate { get; set; }
+        internal double total_Amt { get; set; }
+    }
+}
